8c427020ab3a5143a6dfc297e70ba4f4c833e9f6
ItsEddy <yukkcake@gmail.com>
2014-08-24 01:18:51 +0800

    Cleaned layout, removed unused lang.

--- site/module/Application/config/module.config.php
+++ site/module/Application/config/module.config.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Zend Framework (http://framework.zend.com/)
  *
@@ -6,41 +7,50 @@
  * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
-
 return array(
-    'router' => array(
-        'routes' => array(
-            'home' => array(
-                'type' => 'Zend\Mvc\Router\Http\Literal',
-                'options' => array(
-                    'route'    => '/',
-                    'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
-                        'action'     => 'index',
-                    ),
-                ),
-            ),
-        ),
-    ),
-    'controllers' => array(
-        'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
-        ),
-    ),
-    'view_manager' => array(
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
-        'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
-        ),
-        'template_path_stack' => array(
-            __DIR__ . '/../view',
-        ),
-    ),
+	'router' => array(
+		'routes' => array(
+			'home' => array(
+				'type' => 'Zend\Mvc\Router\Http\Literal',
+				'options' => array(
+					'route' => '/',
+					'defaults' => array(
+						'controller' => 'Application\Controller\Index',
+						'action' => 'index',
+					),
+				),
+			),
+		),
+	),
+	'translator' => array(
+		'translation_file_patterns' => array(
+			array(
+				'type' => 'gettext',
+				'base_dir' => __DIR__ . '/../language',
+				'pattern' => '%s.mo',
+			),
+		),
+		'locale' => 'zh_TW',
+	),
+	'controllers' => array(
+		'invokables' => array(
+			'Application\Controller\Index' => 'Application\Controller\IndexController'
+		),
+	),
+	'view_manager' => array(
+		'display_not_found_reason' => true,
+		'display_exceptions' => true,
+		'doctype' => 'HTML5',
+		'not_found_template' => 'error/404',
+		'exception_template' => 'error/index',
+		'template_map' => array(
+			'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+			'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+			'error/404' => __DIR__ . '/../view/error/404.phtml',
+			'error/index' => __DIR__ . '/../view/error/index.phtml',
+		),
+		'template_path_stack' => array(
+			__DIR__ . '/../view',
+		),
+	),
 );
